374abc87bd8d26b5af6d54c54da153d90620f4f6
yuanwullink30hub <yuanwullink30@gfl.community>
2025-12-11 16:57:34 +0100

    Add test video URL for localhost development

--- src/data/mobile/App.js
+++ src/data/mobile/App.js
@@ -161,7 +161,7 @@ const MobileAppContent = ({ darkMode, setDarkMode, data, scrollDirection }) => {
                 muted
                 playsInline
               >
-                <source src={headerBgVid} type="video/mp4" />
+                <source src="https://commondatastorage.googleapis.com/gtv-videos-library/sample/sintel.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             </div>
